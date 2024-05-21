bad3b5a7d0e3e749892570587183d9a271d55718
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-22 00:48:52 +0330

    create : mapping profile for product

---
+++ OrderShopCart/Src/Server/OrderShopCart.Server/Endpoints/Product/GetList/Mapper.cs
@@ -0,0 +1,17 @@
+﻿using Mapster;
+using OrderShopCart.Application.CommandAndQuery;
+using OrderShopCart.Dto;
+
+namespace OrderShopCart.Server.Endpoints.Product;
+
+public class GetProductsListMappingProfile : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.ForType<GetProductsListRequest, GetProductListQuery>();
+
+        config.ForType<Domain.Aggregates.Product, ProductDto>()
+                .Map(p => p.Id, src => src.Id.Value.ToString())
+                .Map(p => p.Tags, src => src.Tags.Select(t => t.Value));
+    }
+}
