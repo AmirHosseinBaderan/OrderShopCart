1bb7612274c99fa713781f50176f9529f6a3c62e
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-21 23:16:38 +0330

    create : db context

---
+++ OrderShopCart/Src/Infrastructure/OrderShopCart.Infrastructure/Presistance/DbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderShopCart.Infrastructure.Presistance
+{
+    internal class DbContext
+    {
+    }
+}
