8363a6bbb75c5b58804ddd7959861e0a57557dec
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-22 00:14:05 +0330

    create : dependency injection for application layer

---
+++ OrderShopCart/Src/Application/OrderShopCart.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace OrderShopCart.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection ConfigureApplicationLayer(this IServiceCollection services)
+    {
+
+        var application = typeof(IAssemblyMarker);
+        services.AddMediatR(config => config.RegisterServicesFromAssembly(application.Assembly));
+
+        return services;
+    }
+}
