f09de2d14d260f45dd2468b531c978cd5ee19a88
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-22 00:31:11 +0330

    create : get products query

---
+++ OrderShopCart/Src/Application/OrderShopCart.Application/CommandAndQuery/Product/Query/GetList/GetProductListQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using OrderShopCart.Domain.Aggregates;
+
+namespace OrderShopCart.Application.CommandAndQuery;
+
+public record GetProductListQuery(int Page, int Count) : IRequest<IEnumerable<Product>>;
---
+++ OrderShopCart/Src/Application/OrderShopCart.Application/CommandAndQuery/Product/Query/GetList/GetProductListQueryHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using OrderShopCart.Domain.Aggregates;
+using OrderShopCart.Domain.Common;
+
+namespace OrderShopCart.Application.CommandAndQuery;
+
+public class GetProductListQueryHandler(IBaseQuery<Product> query) : IRequestHandler<GetProductListQuery, IEnumerable<Product>>
+{
+    public async Task<IEnumerable<Product>> Handle(GetProductListQuery request, CancellationToken cancellationToken)
+        => await query.GetAllAsync(request.Page, request.Count);
+}
