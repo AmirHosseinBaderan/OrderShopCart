9ea8cd38cb8c8d12c1ab76b4175a0b3d0f08c3af
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-22 00:02:17 +0330

    create : endpoint handler

---
+++ OrderShopCart/Src/Server/OrderShopCart.Server/Abstraction/IEnpointHandler.cs
@@ -0,0 +1,9 @@
+﻿using MapsterMapper;
+using MediatR;
+
+namespace OrderShopCart.Server.Abstraction;
+
+public interface IEnpointHandler<TRequest, TResponse>
+{
+    Task<TResponse> HandlerAsync(TRequest request, IMediator mediator, IMapper mapper);
+}
