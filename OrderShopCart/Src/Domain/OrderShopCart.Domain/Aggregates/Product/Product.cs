0483ac5b212f32187aa68a7cf95812dfb55e8647
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-21 23:01:55 +0330

    create : product domain

---
+++ OrderShopCart/Src/Domain/OrderShopCart.Domain/Aggregates/Product/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace OrderShopCart.Domain.Aggregates;
+
+public class Product : AggregateRootBase
+{
+
+    public Product()
+    {
+        _tags = [];
+    }
+
+    private readonly IEnumerable<Tag> _tags = null!;
+
+
+    public ICollection<Tag> Tags => _tags.ToList();
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+
+}
---
+++ OrderShopCart/Src/Domain/OrderShopCart.Domain/Aggregates/Product/Tag.cs
@@ -0,0 +1,22 @@
+﻿
+namespace OrderShopCart.Domain.Aggregates;
+
+public class Tag : ValueObject<Tag>
+{
+    public string Value { get; set; } = null!:
+
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+
+    public static Tag Create(string tagValue)
+        => new()
+        {
+            Value = tagValue,
+        };
+
+    public override string ToString()
+        => Value;
+}
