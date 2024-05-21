0f40288e59e40a81d7131eba761e7f20063fd498
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-22 00:14:16 +0330

    create : get products endpoint

---
+++ OrderShopCart/Src/Server/OrderShopCart.Server/Endpoints/Product/GetList/GetList.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace OrderShopCart.Server.Endpoints.Product;
+
+public class GetList : IEndpoint, IEnpointHandler<GetProductsListRequest, GetProductsListResponse>
+{
+    public Task<GetProductsListResponse> HandlerAsync(GetProductsListRequest request, IMediator mediator, IMapper mapper)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/product/getlist",
+                async ([AsParameters] GetProductsListRequest request,
+                IMapper mapper,
+                IMediator mediator) =>
+                    await HandlerAsync(request,
+                    mediator,
+                    mapper)
+                ).WithTags(EndpointSchema.ProductsTag);
+    }
+}
---
+++ OrderShopCart/Src/Server/OrderShopCart.Server/Endpoints/Product/GetList/Request.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderShopCart.Server.Endpoints.Product;
+
+public record GetProductsListRequest([FromQuery] int Page, [FromQuery] int Count);
