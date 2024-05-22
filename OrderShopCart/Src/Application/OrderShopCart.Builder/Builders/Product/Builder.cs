61676fca7f7ed0c87d98fd19e17d724a92b149ac
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-22 14:29:51 +0330

    create : product builder

---
+++ OrderShopCart/Src/Application/OrderShopCart.Builder/Builders/Product/Builder.cs
@@ -0,0 +1,27 @@
+﻿namespace OrderShopCart.Builder;
+
+public class ProductBuilder
+{
+    private readonly Product _product = new();
+
+    public ProductBuilder Create(string title, string description, decimal price, IEnumerable<string> tags)
+         => CreateDraft(title, description, price)
+                .AddTags(tags);
+
+    public ProductBuilder CreateDraft(string title, string description, decimal price)
+    {
+        _product.Title = title;
+        _product.Description = description;
+        _product.Price = price;
+
+        return this;
+    }
+
+    public ProductBuilder AddTags(IEnumerable<string> tags)
+    {
+        _product.Tags.AddRange(tags.Select(Tag.Create));
+        return this;
+    }
+
+    public Product Export() => _product;
+}
