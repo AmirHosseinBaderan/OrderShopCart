75c827027a46b5e500e3cb09fe699b042d617d1c
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-22 11:39:49 +0330

    create : create product command

---
+++ OrderShopCart/Src/Application/OrderShopCart.Application/CommandAndQuery/Product/Command/Create/CreateProductCommand.cs
@@ -0,0 +1,7 @@
+﻿using LanguageExt;
+using MediatR;
+using OrderShopCart.Domain.Aggregates;
+
+namespace OrderShopCart.Application.CommandAndQuery;
+
+public record CreateProductCommand(string Title, string Description, decimal Price, IEnumerable<string> Tags) : IRequest<Either<ProductActionStatus, Product>>;
---
+++ OrderShopCart/Src/Application/OrderShopCart.Application/CommandAndQuery/Product/Command/Create/CreateProductCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using OrderShopCart.Builder.Implementation;
+
+namespace OrderShopCart.Application.CommandAndQuery;
+
+public class CreateProductCommandHandler(IBaseCud<Product> cud) : IRequestHandler<CreateProductCommand, Either<ProductActionStatus, Product>>
+{
+    public async Task<Either<ProductActionStatus, Product>> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        ProductBuilder builder = new();
+        ProductDirector director = new(builder);
+
+        director.BuildProduct(new(request.Title, request.Price, request.Description), request.Tags);
+        Product product = builder.GetProduct();
+
+        return await cud.InsertAsync(product) ?
+                product : ProductActionStatus.Failed;
+    }
+}
