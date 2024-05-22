c771015b921c9e6a414dd693d98b03d949286adb
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-22 11:39:28 +0330

    fix : change name of model

---
+++ OrderShopCart/Src/Server/OrderShopCart.Server/Abstraction/IEndpointHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace OrderShopCart.Server.Abstraction;
+
+public interface IEndpointHandler<TRequest>
+{
+    Task<ApiModel> HandlerAsync(TRequest request, IMediator mediator, IMapper mapper);
+}
--- OrderShopCart/Src/Server/OrderShopCart.Server/Abstraction/IEnpointHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using MapsterMapper;
-using MediatR;
-
-namespace OrderShopCart.Server.Abstraction;
-
-public interface IEnpointHandler<TRequest, TResponse>
-{
-    Task<TResponse> HandlerAsync(TRequest request, IMediator mediator, IMapper mapper);
-}
