3cbe48750079e1abce0bf5d243f9396bac9c3e88
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-25 10:53:14 +0330

    create : product action result

---
+++ OrderShopCart/Src/Server/OrderShopCart.Server/Endpoints/Product/ActionStatus.cs
@@ -0,0 +1,15 @@
+﻿using OrderShopCart.Application.CommandAndQuery;
+
+namespace OrderShopCart.Server.Endpoints;
+
+public static class ProductsActionResult
+{
+    public static ApiModel ToApiResult(this ProductActionStatus status)
+            => status switch
+            {
+                ProductActionStatus.Success => Success("عملیات با موفقیت انجام شد", new { }),
+                ProductActionStatus.Failed => ApiException(),
+                ProductActionStatus.NotFound => Faild(404, "موردی یافت نشد"),
+                _ => ApiException(),
+            };
+}
