629ea87df61429b5139b55c49b678eeb8fa7678f
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-25 11:04:02 +0330

    create : validator filter

---
+++ OrderShopCart/Src/Server/OrderShopCart.Server/Endpoints/Product/Create/Validator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace OrderShopCart.Server.Endpoints.Product;
+
+public class CreateProductValidator : AbstractValidator<CreateProductRequest>
+{
+    public CreateProductValidator()
+    {
+        RuleFor(p => p.Price)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("قیمت محصول را وارد کنید");
+
+        RuleFor(p => p.Title)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("عنوان محصول را وارد کنید")
+            .MaximumLength(100)
+            .WithMessage("عنوان مصحول نباید بیشتر از 100 کاراکتر باشد");
+
+        RuleFor(p => p.Description)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("برای محصول توضیحاتی اضافه کنید");
+    }
+}
