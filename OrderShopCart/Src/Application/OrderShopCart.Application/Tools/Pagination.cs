d84e204102605069ebf8a6d6491e6aed62746b4c
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-28 14:34:13 +0330

    create : pagination tools and model

---
+++ OrderShopCart/Src/Application/OrderShopCart.Application/Tools/Pagination.cs
@@ -0,0 +1,20 @@
+﻿using OrderShopCart.Dto;
+
+namespace OrderShopCart.Application.Tools;
+
+public static class PaginationExtensions
+{
+    public static BasePaginationResponse<TModel> ToPagination<TModel>(this IEnumerable<TModel> items, int itemsCount, int pageSize)
+        => new(items,
+            itemsCount.ToPageCount(pageSize),
+            itemsCount);
+
+    public static int ToPageCount(this int itemsCount, int pageSize)
+    {
+        if (itemsCount is 0 || pageSize is 0) return itemsCount;
+        var pageCount = itemsCount / pageSize;
+        if (itemsCount % pageSize != 0)
+            pageCount++;
+        return pageCount;
+    }
+}
---
+++ OrderShopCart/Src/Domain/OrderShopCart.Dto/Common/Pagination.cs
@@ -0,0 +1,3 @@
+﻿namespace OrderShopCart.Dto;
+
+public record BasePaginationResponse<TResult>(IEnumerable<TResult> Result, int PagesCount, int TotalItemsCount);
