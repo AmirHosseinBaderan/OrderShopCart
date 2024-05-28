cc7d7db3f13d3de2cb7d70561423db7d95ebd82e
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-28 13:45:07 +0330

    create : group entity

---
+++ OrderShopCart/Src/Domain/OrderShopCart.Domain/Aggregates/Group/Group.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderShopCart.Domain.Aggregates;
+
+public class Group : AggregateRootBase
+{
+    public string Name { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+}
