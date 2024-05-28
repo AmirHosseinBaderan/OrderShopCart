db4a40908b78c94aca4d5b5c7b84cf65b63c0fa4
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-28 14:01:02 +0330

    update : set many to many releationships

--- OrderShopCart/Src/Domain/OrderShopCart.Domain/Aggregates/Group/Group.cs
+++ OrderShopCart/Src/Domain/OrderShopCart.Domain/Aggregates/Group/Group.cs
@@ -2,7 +2,22 @@
 
 public class Group : AggregateRootBase
 {
+    public Group()
+    {
+        _products = [];
+    }
+
+    private readonly List<Product> _products = null!;
+
+    public IReadOnlyList<Product> Products => _products;
+
     public string Name { get; set; } = null!;
 
     public string Title { get; set; } = null!;
+
+    public void AddProduct(Product product)
+        => _products.Add(product);
+
+    public void AddProductRange(IEnumerable<Product> products)
+        => _products.AddRange(products);
 }
--- OrderShopCart/Src/Domain/OrderShopCart.Domain/Aggregates/Product/Product.cs
+++ OrderShopCart/Src/Domain/OrderShopCart.Domain/Aggregates/Product/Product.cs
@@ -6,11 +6,16 @@ public class Product : AggregateRootBase
     public Product()
     {
         _tags = [];
+        _groups = [];
     }
 
     private readonly List<Tag> _tags = null!;
 
-    public List<Tag> Tags => _tags.ToList();
+    private readonly List<Group> _groups = null!;
+
+    public IReadOnlyList<Tag> Tags => _tags;
+
+    public IReadOnlyList<Group> Groups => _groups;
 
     public string Title { get; set; } = null!;
 
@@ -23,4 +28,10 @@ public class Product : AggregateRootBase
 
     public void AddTagRange(IEnumerable<Tag> tag)
        => _tags.AddRange(tag);
+
+    public void AddGroup(Group group)
+        => _groups.Add(group);
+
+    public void AddGroupRange(IEnumerable<Group> group)
+        => _groups.AddRange(group);
 }
