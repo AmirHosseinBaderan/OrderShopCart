38a52cc34bd39332adf16a641cc0b82b361bc970
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-28 14:33:37 +0330

    create : get groups list request and endpoint

---
+++ OrderShopCart/Src/Server/OrderShopCart.Server/Endpoints/Group/GetList/GetList.cs
@@ -0,0 +1,33 @@
+﻿
+
+using OrderShopCart.Application.CommandAndQuery;
+using OrderShopCart.Dto;
+
+namespace OrderShopCart.Server.Endpoints.Group;
+
+public class GetGroupsListEndpoint : IEndpoint, IEndpointHandler<GetGroupsListRequest>
+{
+    public async Task<ApiModel> HandlerAsync(GetGroupsListRequest request, IMediator mediator, IMapper mapper)
+    {
+        GetGroupsListQuery command = mapper.Map<GetGroupsListQuery>(request);
+        var result = await mediator.Send(command);
+        return Success("", new
+        {
+            Groups = mapper.Map<GroupDto>(result.Result),
+            result.PagesCount,
+            result.TotalItemsCount,
+        });
+    }
+
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/group/getlist", async
+                ([AsParameters] GetGroupsListRequest request,
+                IMediator mediator,
+                IMapper mapper)
+                => await HandlerAsync(request,
+                mediator,
+                mapper))
+            .WithTags(EndpointSchema.GroupsTag);
+    }
+}
---
+++ OrderShopCart/Src/Server/OrderShopCart.Server/Endpoints/Group/GetList/Request.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderShopCart.Server.Endpoints.Group;
+
+public record GetGroupsListRequest([FromQuery] int Page, [FromQuery] int Count);
